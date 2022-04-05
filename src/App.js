d0d7e43b91898c828fc9e5f338edc85b62a8d263
mihmsical <103008061+mihmsical@users.noreply.github.com>
2022-04-04 20:11:54 -0500

    added name
    Added my name to the file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {movies} from './movies';
 const App = () => {
     return (
         <div>
+		<p>Sarah Mihm</p>
             <pre>{JSON.stringify(movies, null, 4)}</pre>
         </div>
     );
