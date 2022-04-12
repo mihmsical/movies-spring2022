7f1e0ae69206715de2feef0e4c8c12b0f60097e7
mihmsical <sarah.mihm12@gmail.com>
2022-04-11 20:49:15 -0500

    utilizing state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
 import {movies} from './movies';
 import MoviePreview from "./MoviePreview";
+import {useState} from "react";
 
 const App = () => {
+	
+	/* function useState(startingState) {
+		some logic goes here
+		
+		return [variable, setVariableFunction];
+	}
+	*/
+	
+	const [currentMovie, setCurrentMovie] = useState(null);
+	
     return (
         <div className="movie-container">
 			{
