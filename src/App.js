4bda60d0eea7a0fc943c692f4decf8d907fd0b2c
mihmsical <sarah.mihm12@gmail.com>
2022-04-11 19:30:21 -0500

    Debugging fix
    Fixing a bug with mapping

--- src/App.js
+++ src/App.js
@@ -5,20 +5,13 @@ const App = () => {
     return (
         <div>
 			{
-				movies.map((movie) => <p>{movie.title}</p>)
+				movies.map((movie) => 
+					<>
+						<p>{movie.title}</p>
+						<img src={movie.posterUrl}/>
+					</>
+				)
 			}
-		<p>{movies[0].title</p>
-		<p>{movies[1].title</p>
-		<p>{movies[2].title</p>
-		<p>{movies[3].title</p>
-		<p>{movies[4].title</p>
-		<p>{movies[5].title</p>
-		<p>{movies[6].title</p>
-		<p>{movies[7].title</p>
-		<p>{movies[8].title</p>
-		<p>{movies[9].title</p>
-		<p>Sarah Mihm</p>
-            <pre>{JSON.stringify(movies, null, 4)}</pre>
         </div>
     );
 }
